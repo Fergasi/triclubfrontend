77e63954c66431f6e038d577e3009138937fbbea
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-08-19 15:02:24 -0600

    create program

---
+++ src/Hooks/Programs.js
@@ -0,0 +1,30 @@
+// Heroku
+// const urlEndpoint = process.env.REACT_APP_DATABASE_URL;
+
+//LOCAL
+const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
+
+//Function to get Programs to display on UserHomePage
+export const getActivePrograms = async () => {
+  const url = `${urlEndpoint}/programs/get-active-programs`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const responseJSON = await response.json();
+  return responseJSON;
+};
+
+export const submitProgram = async (programData) => {
+  const response = await fetch(`${urlEndpoint}/programs/submit-program`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(programData),
+  });
+  const responseJSON = await response.json();
+  return responseJSON;
+};
